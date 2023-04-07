d630474bf6545ed15438afce9934bb330cbf7113
Anna <ausviridova@gmail.com>
2023-04-07 07:48:41 +0400

    Был создан бин на основе класса Cat, далее произведено сравнение экземпляров HelloWorld и Cat по ссылке, выполнена проверка тестовым методом.

---
+++ src/main/java/Cat.java
@@ -0,0 +1,30 @@
+public class Cat {
+    private String moniker;
+    private String breed;
+    private int age;
+
+    public String getMoniker() {
+        return moniker;
+    }
+
+    public void setMoniker(String message) {
+        this.moniker = moniker;
+    }
+
+    public String getBreed() {
+        return breed;
+    }
+
+    public void setBreed(String message) {
+        this.breed = breed;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+}
